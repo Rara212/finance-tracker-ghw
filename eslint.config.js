c84b316df55edb9e745b1e5f0742cc1cc5db3c00
Mutya Mustafa <mutya2112@gmail.com>
2026-01-05 22:05:09 +0700

    updated files

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
@@ -121,3 +122,28 @@ export default [
   // Apply prettier config last to override any conflicting rules
   prettierConfig,
 ];
+=======
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { defineConfig, globalIgnores } from 'eslint/config'
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+  },
+])
+>>>>>>> Stashed changes
